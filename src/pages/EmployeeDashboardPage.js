17556933dcd218dcc46aa35f884738922f30324d
abderrahmaneg <105305990+abderrahmaneg@users.noreply.github.com>
2023-06-05 19:38:56 +0100

    Add files via upload

---
+++ src/pages/EmployeeDashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeeDashboar from '../components/employee/EmployeeDashboard';
+
+const EmployeeDashboardPage = () => {
+  return (
+    <div>
+ <EmployeeDashboar />
+    </div>
+  );
+};
+
+export default EmployeeDashboardPage;
